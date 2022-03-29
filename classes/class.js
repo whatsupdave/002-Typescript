9e8c9fc746e4939d3a2450c2ff222646aac8a9c7
Eivydas Kantautas <kantautaseivydas@gmail.com>
2022-03-29 22:13:14 +0300

    class update

--- classes/class.js
+++ classes/class.js
@@ -1,4 +1,30 @@
-var jonas = {
-    vardas: 'jonas',
-    amzius: 21
-};
+var Zmogus = /** @class */ (function () {
+    function Zmogus(_vardas, _amzius, _email) {
+        this._vardas = _vardas;
+        this._amzius = _amzius;
+        this._email = _email;
+    }
+    Zmogus.prototype.isvedimas = function () {
+        console.log("Aš esu: " + this._vardas + " mano amzius: " + this._amzius);
+    };
+    Object.defineProperty(Zmogus.prototype, "amzius", {
+        get: function () {
+            return this._amzius;
+        },
+        set: function (amzius) {
+            if (amzius < 18)
+                throw new Error("Amzius per mazas");
+            this._amzius = amzius;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return Zmogus;
+}());
+var jonas = new Zmogus("Jonas", 32, "jonas@gmail.com");
+var petras = new Zmogus("Petras", 22);
+// petras.setAmzius(19);
+petras.amzius = 10;
+console.log(petras.amzius);
+jonas.isvedimas();
+petras.isvedimas();
